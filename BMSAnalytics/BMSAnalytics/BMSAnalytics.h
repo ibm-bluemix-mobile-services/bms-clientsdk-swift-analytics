8254b1423a1dd52dcfce4abb7f2205f91711d622
Anthony Oliveri <oliveri@us.ibm.com>
2016-01-19 13:56:17 -0600

    Started new Xcode project

---
+++ BMSAnalytics/BMSAnalytics/BMSAnalytics.h
@@ -0,0 +1,19 @@
+//
+//  BMSAnalytics.h
+//  BMSAnalytics
+//
+//  Created by Anthony Oliveri on 1/19/16.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BMSAnalytics.
+FOUNDATION_EXPORT double BMSAnalyticsVersionNumber;
+
+//! Project version string for BMSAnalytics.
+FOUNDATION_EXPORT const unsigned char BMSAnalyticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BMSAnalytics/PublicHeader.h>
+
+
