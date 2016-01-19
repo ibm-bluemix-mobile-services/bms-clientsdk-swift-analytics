b1705cb7e58b48e5a0b733dded0cd2ba5e684020
Anthony Oliveri <oliveri@us.ibm.com>
2016-01-19 14:06:46 -0600

    Added targets for watchOS framework, iOS test app, and watchOS test app

---
+++ BMSAnalytics/BMSAnalyticsWatchOS/BMSAnalyticsWatchOS.h
@@ -0,0 +1,19 @@
+//
+//  BMSAnalyticsWatchOS.h
+//  BMSAnalyticsWatchOS
+//
+//  Created by Anthony Oliveri on 1/19/16.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for BMSAnalyticsWatchOS.
+FOUNDATION_EXPORT double BMSAnalyticsWatchOSVersionNumber;
+
+//! Project version string for BMSAnalyticsWatchOS.
+FOUNDATION_EXPORT const unsigned char BMSAnalyticsWatchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BMSAnalyticsWatchOS/PublicHeader.h>
+
+
