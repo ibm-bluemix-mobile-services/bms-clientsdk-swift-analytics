f1793e404aed90bb5e3c146b2966f17ad23da7eb
Anthony Oliveri <oliveri@us.ibm.com>
2016-01-20 16:36:19 -0600

    New Error enum for all BMSAnalytics-specific errors

---
+++ Source/Error.swift
@@ -0,0 +1,25 @@
+/*
+*     Copyright 2015 IBM Corp.
+*     Licensed under the Apache License, Version 2.0 (the "License");
+*     you may not use this file except in compliance with the License.
+*     You may obtain a copy of the License at
+*     http://www.apache.org/licenses/LICENSE-2.0
+*     Unless required by applicable law or agreed to in writing, software
+*     distributed under the License is distributed on an "AS IS" BASIS,
+*     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*     See the License for the specific language governing permissions and
+*     limitations under the License.
+*/
+
+import BMSCore
+
+// TODO: Rename error code type
+
+/**
+    These error codes indicate a failure that occurred within the BMSAnalytics framework.
+ */
+public enum AnalyticsErrorCode: Int {
+    
+    // Start at 100 so that BMSAnalytics errors are distinguishable from BMSCore errors (which start at 0)
+    case AnalyticsNotInitialized = 100
+}
