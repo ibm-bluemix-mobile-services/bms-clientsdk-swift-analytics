8d7015ba6951a1d930c3adf357091aec7d0ef9a4
Anthony Oliveri <oliveri@us.ibm.com>
2016-02-10 18:38:23 -0600

    [Test apps] Added view for displaying stored logs

---
+++ Tests/Test Apps/TestAppiOS/LogDisplayViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LogDisplayViewController.swift
+//  BMSAnalytics
+//
+//  Created by Anthony Oliveri on 2/10/16.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+import UIKit
+
+class LogDisplayViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
