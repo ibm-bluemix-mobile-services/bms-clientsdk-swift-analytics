6171e5b2c5e51c0abb316ea0b02eecf115fe9a92
Anthony Oliveri <oliveri@us.ibm.com>
2016-04-07 15:47:53 -0500

    Removed Pods/ directory from version control

--- Pods/Target Support Files/BMSCore-iOS/BMSCore-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BMSCore_iOS : NSObject
-@end
-@implementation PodsDummy_BMSCore_iOS
-@end
--- Pods/Target Support Files/BMSCore-watchOS/BMSCore-watchOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BMSCore_watchOS : NSObject
-@end
-@implementation PodsDummy_BMSCore_watchOS
-@end
